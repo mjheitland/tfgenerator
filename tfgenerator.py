7a8e1c54fd9c2c184bb0566c52bb7d5605a7be0e
Michael Heitland <heitlm@amazon.de>
2020-05-09 11:28:24 +0200

    Initial version

--- tfgenerator.py
+++ tfgenerator.py
@@ -9,10 +9,18 @@ import shutil
 import yaml
 from pathlib import Path
 
+def recursive_overwrite(src, dest):
+    if os.path.isdir(src):
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        files = os.listdir(src)
+        for f in files:
+            recursive_overwrite(os.path.join(src, f), os.path.join(dest, f))
+    else:
+        shutil.copyfile(src, dest)
+
 def main():
-  pass
-
-
+    recursive_overwrite("templates", "out")
 
 if __name__ == "__main__":
   main()
